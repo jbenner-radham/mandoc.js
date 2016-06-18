699113f62ae00d8c97a3b4d01e76ec02acd0e265
James Benner <james.benner@gmail.com>
2016-06-18 18:16:19 -0400

    More initial scaffolding.

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 const fs     = require('fs');
 const mandoc = require('./lib');
 
-let file   = process.argv[2];
-let source = fs.readFileSync(file).toString();
+let file = process.argv[2];
+let src  = fs.readFileSync(file).toString();
 
-mandoc.hello();
+mandoc.parse(src);
